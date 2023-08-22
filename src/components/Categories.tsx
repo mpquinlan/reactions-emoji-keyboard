2ae7cd405f355f2c97a37020ce541039df144daf
Michael Quinlan <41512991+mpquinlan@users.noreply.github.com>
2023-08-22 11:17:37 -0400

    Update Categories.tsx

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -20,28 +20,15 @@ export const Categories = (p: Props) => {
     onCategoryChangeFailed,
     categoryPosition,
     renderList,
-    setActiveCategoryIndex,
-    clearEmojiTonesData,
     theme,
     styles: themeStyles,
     enableCategoryChangeAnimation,
     setShouldAnimateScroll,
   } = React.useContext(KeyboardContext)
   const scrollNav = React.useRef(new Animated.Value(0)).current
-  const handleScrollToCategory = React.useCallback(
-    (category: CategoryTypes) => {
-      clearEmojiTonesData()
-      setActiveCategoryIndex(renderList.findIndex((cat) => cat.title === category))
-      setShouldAnimateScroll(enableCategoryChangeAnimation)
-    },
-    [
-      clearEmojiTonesData,
-      setActiveCategoryIndex,
-      renderList,
-      setShouldAnimateScroll,
-      enableCategoryChangeAnimation,
-    ]
-  )
+  const handleScrollToCategory = React.useCallback(() => {
+    setShouldAnimateScroll(enableCategoryChangeAnimation)
+  }, [setShouldAnimateScroll, enableCategoryChangeAnimation])
 
   const renderItem = React.useCallback(
     ({ item, index }: { item: CategoryNavigationItem; index: number }) => (
